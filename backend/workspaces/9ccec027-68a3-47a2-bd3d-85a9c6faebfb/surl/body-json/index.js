2007cb8c101afb986a1ccd5677e260e267168651
Stephen Akinyemi <appcypher@outlook.com>
2021-10-28 14:45:37 +0100

    Change surl syntax to use worker.js spec
    - Add more samples

--- backend/workspaces/9ccec027-68a3-47a2-bd3d-85a9c6faebfb/surl/body-json/index.js
+++ backend/workspaces/9ccec027-68a3-47a2-bd3d-85a9c6faebfb/surl/body-json/index.js
@@ -1,21 +1,3 @@
-function test() {
-  return {
-    headers: {
-      "Content-Type": ["application/json", "charset=utf-8"],
-    },
-    data: `{
-      "name":"Tamedun La",
-      "age": 22,
-      "email": "tamedum.la@gmail.com",
-      "address": {
-        "street": "21st, tamedun st.",
-        "city": "Ikoyi",
-        "state": "Lagos",
-        "country": "Nigeria"
-      }
-    }`,
-    statusCode: 200,
-  };
-}
-
-test();
+self.addEventListener('fetch', event => {
+  event.respondWith(new Response("Hello world"));
+})
