345df43c14ad6cac3c089d79c8fcd510182d7df7
Stephen Akinyemi <appcypher@outlook.com>
2022-01-08 10:03:52 +0100

    Update apps/static code

--- workspaces/unreachable/api/v1/system/apps/=/static/index.get.js
+++ workspaces/unreachable/api/v1/system/apps/=/static/index.get.js
@@ -5,20 +5,31 @@ const {
   events: { http },
 } = Tera;
 
-let path = http.request.uri.query.get("path") || "index.html";
-const app = http.request.uri.path.get("apps");
-if (app == null) {
-  throw Error("Must specify app query");
+async function main() {
+  let path = http.request.uri.query.get("path") || "index.html";
+
+  // Get the app name from url path.
+  const app = http.request.uri.path.get("apps");
+  if (app == null) {
+    throw Error("Must specify app query");
+  }
+
+  // Set filename.
+  const filename = `/apps/${app}/dist/${path}`;
+
+  log.info(`>>>>> Serving file ${filename}`);
+
+  // Open file
+  const file = await File.open(filename, { read: true });
+
+  // Set response and send content of file as response
+  let response = new Response(file, {
+    headers: { "Access-Control-Allow-Origin": "http://localhost:3000" },
+  });
+
+  await http.respondWith(response);
 }
 
-const appPath = `/apps/${app}/dist/${path}`;
-
-const file = await File.open(appPath, { read: true });
-
-let response = new Response(file, {
-  headers: {
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-  },
-});
-
-await http.respondWith(response);
+if (import.meta.main) {
+  await main();
+}
